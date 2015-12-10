a9d671bd8e2ee1b3d9c59a3bc65c608f71cd2505
chloelizabeth <chloelizabethboyko@gmail.com>
2015-12-10 09:41:57 -0500

    add successful carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,18 +1,20 @@
 
-$(document).ready(function() {
 
- $('.fade').slick({
-    dots: true,
-    infinite: true,
-    speed: 700,
-    autoplay:true,
-    autoplaySpeed: 2000,
-    arrows:false,
-    slidesToShow: 1,
-    slidesToScroll: 1
- });
+$(document).ready(function(){
 
- });
+$('.fashion').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true
+});
+     
+
+
+});
+  
 
 
 //$(function(){
